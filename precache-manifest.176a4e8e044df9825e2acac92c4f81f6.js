d4127571ab1fb1f7bd9c3dc93c80445524b661d5
Paweł Chmiel <pawelchmiel7@gmail.com>
2020-05-08 12:29:48 +0200

    Updates

---
+++ precache-manifest.176a4e8e044df9825e2acac92c4f81f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa1bb4517e02ee531750b5884530501",
+    "url": "/task-app/index.html"
+  },
+  {
+    "revision": "0dbe3587f2897ac75496",
+    "url": "/task-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "dde4bd20ab7ac492ed81",
+    "url": "/task-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0dbe3587f2897ac75496",
+    "url": "/task-app/static/js/2.124d3d80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-app/static/js/2.124d3d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde4bd20ab7ac492ed81",
+    "url": "/task-app/static/js/main.bc05c25e.chunk.js"
+  },
+  {
+    "revision": "a1a3449a8e657e461a79",
+    "url": "/task-app/static/js/runtime-main.299c51a9.js"
+  }
+]);
